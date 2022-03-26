bc39af7085f39b703bbf8d7797bc48a5b74621be
jannatnury <jannatnury253@gmail.com>
2022-03-26 20:42:11 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './componants/Header/Header';
 import Product from './componants/Product/Product';
+import Shop from './componants/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Product></Product>
+      <Shop></Shop>
       
     </div>
   );
--- src/componants/Product/Product.js
+++ src/componants/Product/Product.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Product.css'
+
 
 
 const Product = () => {
     return (
         <div>
-            <h1>This is products</h1>
-            <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/fbaf991a78bc4896a3e9ad7800abcec6_9366/Ultraboost_22_Shoes_Black_GZ0127_01_standard.jpg" alt="" />
+            <img src="" alt="" />
         </div>
     );
 };
